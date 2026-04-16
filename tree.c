0f42d574970b2dd633eb0e9bd893e1c6e9187828
Akash Singh <akisini1847u@gmail.com>
2026-04-16 18:24:12 +0530

    Phase 2: added tree serialization for entries

--- tree.c
+++ tree.c
@@ -8,7 +8,7 @@
 //
 // Example single entry (conceptual):
 //   "100644 hello.txt\0" followed by 32 raw bytes of SHA-256
-
+// Initialized tree structure for storing directory entries
 #include "tree.h"
 #include "index.h"
 #include <stdio.h>
