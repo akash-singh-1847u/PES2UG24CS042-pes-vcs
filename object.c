b510be7fb97b9ed78cba94eaca2c4eb7459affff
Akash Singh <akisini1847u@gmail.com>
2026-04-16 17:59:34 +0530

    Phase 1: added directory sharding for object storage

--- object.c
+++ object.c
@@ -18,7 +18,7 @@
 #include <openssl/evp.h>
 
 // ─── PROVIDED ────────────────────────────────────────────────────────────────
-
+// phase 1 progress
 void hash_to_hex(const ObjectID *id, char *hex_out) {
     for (int i = 0; i < HASH_SIZE; i++) {
         sprintf(hex_out + i * 2, "%02x", id->hash[i]);
